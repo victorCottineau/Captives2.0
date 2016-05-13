3eb7747707911355105b127e7ebe066af3099007
Victor Cottineau <Victor Cottineau>
2016-05-13 13:04:46 +0200

    Fixed the loading of the background

--- js/background.js
+++ js/background.js
@@ -1,5 +1,13 @@
+function backgroundOnload() {
+	console.log("Onload");
+	var vid = document.getElementById("video1");
+    vid.src = "video/neutral.mp4";
+    localStorage.setItem(1335, 0);
+    backgroundStart();
+}
+
 function backgroundStart() {
-    console.log("background");
+console.log("background");
 var v = document.getElementById("video1");
 var c = document.getElementById("backgroundCanvas");
 var ctx = c.getContext("2d");
